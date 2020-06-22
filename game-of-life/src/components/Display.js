465c172809f386170bce23d8b445c17373c2776c
CScori <49799016+CScori@users.noreply.github.com>
2020-06-22 15:49:59 -0700

    practice

--- game-of-life/src/components/Display.js
+++ game-of-life/src/components/Display.js
@@ -4,9 +4,7 @@ import Data from './GridData'
 const Display = () => {
     return (
         <div>
-            {Data.map(cell, i) => (
-                <div></div>
-            )}
+            
         </div>
     )
 }
