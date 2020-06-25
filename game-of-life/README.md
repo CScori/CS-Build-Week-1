5b442bfa3b94f87a022edd59d3861f3709d15d89
CScori <49799016+CScori@users.noreply.github.com>
2020-06-25 16:52:39 -0700

    moved in my UPer

--- game-of-life/README.md
+++ game-of-life/README.md
@@ -1,1 +1,20 @@
 # My Understanding
+the rules of the game simulate an ideal population control
+birth can only happen if 3 other cells are living (not 2 as in normal life)
+cells have more conditions to die than to live
+automata are "cognitive" awareness of self and surroundings
+as long as the cell automata can have life the recurssion of new generations will continue
+the gol does not meet criteria for turing complete
+the turing complete NAND is ifinitely alive and is only false if the inputs are true
+double buffering is essentiallay a copy that preps the new life death cycle while the previous is shown 
+
+# My Plan
+Have state/array that holds the cells
+map throu cells
+if cell is false/0 dead otherwise cell is true/1 alive
+visualize life and copy to continue moves in game
+have function to create and scan copy to visualize life and death
+allow stateful updates for cols rows cell color etc
+use state to generate count of how many cycles can be made
+create a random prefiller for the grid
+
--- game-of-life/src/Customs.js
+++ game-of-life/src/Customs.js
@@ -6,31 +6,43 @@ const Customs = (props) => {
     
     
     const handleChange = e => {
+     setColor({ color: e.target.value})
+     setSpeed({ speed: e.target.value})
+     setRow({ row: e.target.value}) 
+     setCol({ col: e.target.value})
+    }
+    const handleSubmit = e => {
+      e.preventDefault()
 
     }
     
     return (
      <div>
-        <form>
-        <label>
-                Rows:
-                <input className="input" type="text" value={rows} onChange={setRow} />
-              </label>
-
-              <label>
-                Columns:
-                <input className="input" type="text" value={cols} onChange={setCol} />
-              </label>
-
-              <label>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>
                 Change speed:
-                <input className="input" type="text" value={speed} onChange={setSpeed} />
+                <input className="input" type="text" value={speed} onChange={handleChange} />
               </label>
 
               <label>
                 Change square color:
-                <input className="input" type="text" value={color} onChange={setColor} />
+                <input className="input" type="text" value={color} onChange={handleChange}  />
               </label>
+              </div>
+              <div> 
+               <label>
+                Rows:
+                <input className="input" type="number" value={rows} onChange={handleChange}  />
+              </label>
+
+              <label>
+                Columns:
+                <input className="input" type="number" value={col} onChange={handleChange}  />
+              </label>
+
+              <input type="submit" value="Submit"/>
+              </div>
         </form>
             
         </div>
