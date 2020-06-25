8bb464773f116eaea8472fe7e86eb05a8fc9f786
CScori <49799016+CScori@users.noreply.github.com>
2020-06-25 13:37:04 -0700

    new buttons && alive determinator

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState } from 'react';
+import React, {useState, useCallback, useRef } from 'react';
 import produce from 'immer'
 
 
@@ -6,8 +6,22 @@ const bRows = 25
 const bCols = 25
 
 const App = () => {
-
-const generate = () => {
+  const bRows = 25
+  const bCols = 25
+  // neigbor checks
+  const operations = [
+    [0,1],
+    [0,-1],
+    [1,-1],
+    [-1,1],
+    [1,1],
+    [-1,-1],
+    [1,0],
+    [-1,0]
+  ]
+//might change to usestate later
+//grid span
+  const generate = () => {
   const rows = []
   for (let i = 0; i < bRows; i++){
     rows.push(Array.from(Array(bCols), () => 0))
@@ -15,33 +29,107 @@ const generate = () => {
      return rows
     }
 
+    const randomizer = () => {
+      const rows = []
+  for (let i = 0; i < bRows; i++){
+    rows.push(Array.from(Array(bCols), () => Math.random() > .7 ? 1 : 0
+    ))
+     }
+     setGrid(rows)
+    }
+  
+
 const [grid, setGrid] = useState(generate)
-console.log('grid', grid)
+const [running, setRunning] = useState(false )
+
+
+const runningRef= useRef(running)
+runningRef.current = running
+
+//kill condition
+const runLife = useCallback(
+  () => {
+    if(!runningRef.current){
+      return
+    }
+    setGrid((g) => {
+      //going through grid start
+      return produce(g, gridCopy => {
+        for(let i=0; i<bRows; i++){
+          for(let j=0; i<bCols; j++){
+            let neighbors = 0
+            //calculating neighbors 
+            operations.forEach(([x,y]) => {
+              const newI = i+x
+              const newJ = j+y
+              if(newI >= 0 && newI< bRows && newJ >=0 && newJ < bCols){
+                neighbors += g[newI][newJ]
+              }
+            })
+            //live or die determine
+            if (neighbors < 2 || neighbors > 3){
+              gridCopy[i][j] = 0
+            } else if(g[i][j] === 0 && neighbors === 3){
+              gridCopy[i][j] = 1
+            } 
+          }
+        }
+      })
+    })
+    
+    setTimeout(runLife, 100)
+  }, [])
+
+
   return (
-    <div style={{
-      display:"grid",
-      gridTemplateColumns: `repeat(${bCols},20px)`
-    }}>
-      {grid.map((rows, z) =>
-      rows.map((cols, a) => (
-        <div
-        key={`${z}-${a}`}
-        onClick={() =>{
-          const newGrid = produce(grid, gridCopy => {
-            gridCopy[z][a] = 1
-          })
-          setGrid(newGrid)
-        }}
-        style={{
-          width: 20,
-          height: 20,
-          backgroundColor: grid[z][a] ? "blue" : undefined,
-          border: 'solid 1px navy'
-        }}
-        />
-      ))
-      )}
-    </div>
+    <>
+    <Headers>
+      <button onClick={() => {
+        setRunning(!running)
+        if(!running){
+          runningRef.current = true
+          runLife()
+        }}}>Start</button>
+
+      <button onClick={()=> {
+        setGrid(generate())}}>Clear</button>
+
+      <button onClick={()=> {
+        randomizer()}}>Random</button>
+
+    </Headers>
+    <Displays>
+      <div>Generation : {}</div>
+    </Displays>
+    <Grid>
+      <div style={{
+        display:"grid",
+        gridTemplateColumns: `repeat(${bCols},20px)`
+      }}>
+        {grid.map((rows, z) =>
+        rows.map((cols, a) => (
+          <div
+          key={`${z}-${a}`}
+          onClick={() =>{
+            const newGrid = produce(grid, gridCopy => {
+              gridCopy[z][a] = grid[z][a] ? 0 : 1
+            })
+            setGrid(newGrid)
+          }}
+          style={{
+            width: 20,
+            height: 20,
+            backgroundColor: grid[z][a] ? "blue" : undefined,
+            border: 'solid 1px navy'
+          }}
+          />
+        ))
+        )}
+      </div>
+    </Grid>
+    
+    
+    </>
   )
 }
 
